1581b541d639c5be88a2ab514979a3aba1eba9fb
Michael Metz <6386318+m-metz@users.noreply.github.com>
2022-05-26 23:58:56 -0600

    Called classes from encryption.py

--- encryption.py
+++ encryption.py
@@ -1,5 +1,5 @@
 # encryption.py
-# AUTHOR NAME
+# Michael Metz
 #
 # A terminal-based encryption application capable of both encoding and decoding text when given a specific cipher.
 # Detailed specifications are provided via the Assignment 3 git repository.
@@ -7,12 +7,23 @@
 # You may import any modules from the standard Python library.
 # Remember to include docstrings and comments.
 
-
+import sys
+from text_codec.command_line_interface import CommandLineInterface
 
 def main():
+    print("")
     print("ENSF 592 Encryption Program")
+    print("Use Ctrl+C to quit.")
+    print("")
 
-    #You may include additional code here.
+    cli = CommandLineInterface()
+    try:
+        cli.prompt_for_encode_decode_mode()
+        cli.prompt_for_cipher()
+        cli.prompt_for_text_to_encode_decode()
+        cli.encode_decode_text()
+    except KeyboardInterrupt:
+        sys.exit()
 
 if __name__ == '__main__':
     main()
